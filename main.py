d37fc3cf790c989027251426ab1c61f41273d316
garrettkmoody <extrillieos@gmail.com>
2021-04-23 20:05:05 -0800

    changed logo

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 accountBalanceInitial = 3129.81
 incomingAccBal = 3500
-users = []
+users = [("Garrett", ".680492", "112233"), ("Ryley", ".319508", "3422")]
+
+stringForGoogle = "Garrett .680492 112233\nRyley .319508 3422"
 
 
 @app.route("/")
@@ -42,18 +44,18 @@ def update():
     else:
         return jsonify({"message": "ERROR: Unauthorized"}), 401
 
-@app.context_processor
-def override_url_for():
-    return dict(url_for=dated_url_for)
+# @app.context_processor
+# def override_url_for():
+#     return dict(url_for=dated_url_for)
 
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(app.root_path,
-                                 endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
+# def dated_url_for(endpoint, **values):
+#     if endpoint == 'static':
+#         filename = values.get('filename', None)
+#         if filename:
+#             file_path = os.path.join(app.root_path,
+#                                  endpoint, filename)
+#             values['q'] = int(os.stat(file_path).st_mtime)
+#     return url_for(endpoint, **values)
 
 def doesUserExist(accountKey):
     for user in users:
@@ -66,13 +68,17 @@ def doesUserExist(accountKey):
 
 
 if __name__ == "__main__":
+
+    
     f = open("accountInfo.txt", "r")
     Lines = f.readlines()
-
-    for line in Lines:
-        name, share, code = line.split()
-        users.append((name, share, code))
-        print("Here is name: {}\n Here is share: {} ,   {}".format(name,share, code))
+    if Lines != None: 
+        for line in Lines:
+            name, share, code = line.split()
+            users.append((name, share, code))
+            print("Here is name: {}\n Here is share: {} ,   {}".format(name,share, code))
+    
+    
 
     
     app.run(debug=True)
